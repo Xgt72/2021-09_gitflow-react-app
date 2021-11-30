980f1aa8ee18a4ab6d0d3111d7bc48247aac7374
cyriacinfo <cyriac.intesse@gmail.com>
2021-11-30 14:49:08 +0100

    liberer mes badges

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Free the badges</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
