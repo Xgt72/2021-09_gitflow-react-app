eaf258e76fcb674e113c52c845519378a981aa48
Xavier Georget <xageorget@gmail.com>
2021-11-30 15:16:47 +0100

    Router react added

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <>
+      <h2>About Us</h2>
+      <p>Hello About Us</p>
+    </>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -6,11 +7,17 @@ export default function Header() {
       <header>
         <nav>
           <ul>
-            <li>Home</li>
-            <li>About Us</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
             <li>Free the badges</li>
             <li>Les problèmes arrivent</li>
-            <li>Contact Us</li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
           </ul>
         </nav>
       </header>
