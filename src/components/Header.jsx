4883c3c4f530d507b7a6b921dc5ff1011bca531f
Xavier Georget <xageorget@gmail.com>
2021-11-30 14:49:19 +0100

    Header component created

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
