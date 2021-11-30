a1d2398a6f8696043ecdb3c246d763e76aff655a
Dieumegard <dieumegard.gregory@gmail.com>
2021-11-30 14:49:47 +0100

    created Header3

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Les problèmes arrivent</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
